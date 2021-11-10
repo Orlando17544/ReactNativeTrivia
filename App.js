8aa06869bad7e00fe7b4df8ef0e003040f42630f
Orlando Canchola <orcj17544@gmail.com>
2021-11-10 14:44:59 -0600

    First commit

---
+++ App.js
@@ -0,0 +1,31 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+const App = () => {
+
+  return (
+	  <View>
+	  	<Text>First commit</Text>
+	  </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+  },
+});
+
+export default App;
