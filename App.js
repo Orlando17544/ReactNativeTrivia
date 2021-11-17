a4e37e2f7f25f39f0e9de11ccc4b7f98c8745831
Orlando Canchola <orcj17544@gmail.com>
2021-11-17 16:36:27 -0600

    Music added

--- App.js
+++ App.js
@@ -9,26 +9,49 @@
 import React from 'react';
 import type {Node} from 'react';
 import {
-  StyleSheet,
-  Text,
-  View,
+	StyleSheet,
+	Text,
+	View,
 } from 'react-native';
 
 import SplashScreen from 'react-native-splash-screen'
 
-const App = () => {
-	SplashScreen.hide();
+SplashScreen.hide();
 
-  return (
-	  <View>
-	  	<Text>First commit</Text>
-	  </View>
-  );
+var Sound = require('react-native-sound');
+
+Sound.setCategory('Playback');
+
+var ocarina = new Sound('ocarina.mp3', Sound.MAIN_BUNDLE, (error) => {
+	if (error) {
+		console.log('failed to load the sound', error);
+		return;
+	} else {
+	// if loaded successfully
+	console.log('duration in seconds: ' + ocarina.getDuration() + 'number of channels: ' + ocarina.getNumberOfChannels());
+
+	ocarina.play();
+	}
+
+});
+
+ocarina.setVolume(50);
+
+// To loop the song
+ocarina.setNumberOfLoops(100);
+
+const App = () => {
+
+	return (
+		<View>
+		<Text>First commit</Text>
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-  container: {
-  },
+	container: {
+	},
 });
 
 export default App;
