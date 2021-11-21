00bd27a119bbbed1c17fc170609f73804157f06d
Orlando Canchola <orcj17544@gmail.com>
2021-11-21 00:45:09 -0600

    Top tab navigator implemented

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -18,7 +18,15 @@ import {
 	Image
 } from 'react-native';
 
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+
+const Tab = createMaterialTopTabNavigator();
+
+import CategoriesScreen from './CategoriesScreen.js';
+import FriendsScreen from './FriendsScreen.js';
 
 const HomeScreen = () => {
 
@@ -29,14 +37,29 @@ const HomeScreen = () => {
 			style={ styles.imageBackgroundContainer }
 			resizeMode="cover"
 		>
-		{/*
 		<Image
 			source={require('../../assets/profile.png')}
-		/>}*/}
+			style={ styles.profileImage }
+			resizeMode="contain"
+		/>
 		</ImageBackground>
-		<TouchableOpacity style={{ flex: 2 }}>
-			<Text>Play</Text>
+		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+		<TouchableOpacity style={{ backgroundColor: '#19ba50', borderRadius: 5, paddingHorizontal: '40%', paddingVertical: '3%' }}>
+			<Text style={{ fontSize: 18, color: 'white' }}>Play</Text>
 		</TouchableOpacity>
+		</View>
+		<Tab.Navigator style={{ flex: 7 }} >
+		      <Tab.Screen name="Categories" component={CategoriesScreen} options={{ tabBarIcon: () => (
+			      <MaterialIcons name="category" size={20} color="#505149" /> 
+		      ), tabBarActiveTintColor: "#505149", tabBarInactiveTintColor: "#8f9c8b", tabBarPressColor: "#505149"
+		      }}
+/>
+		      <Tab.Screen name="Friends" component={FriendsScreen} options={{ tabBarIcon: () => (
+			      <FontAwesome5 name="user-friends" size={20} color="#505149" /> 
+		      ), tabBarActiveTintColor: "#505149", tabBarInactiveTintColor: "#8f9c8b", tabBarPressColor: "#505149"
+		      }}
+/>
+		</Tab.Navigator>
 		</View>
 	);
 };
@@ -47,7 +70,11 @@ const styles = StyleSheet.create({
 		justifyContent: 'center'
 	},
 	imageBackgroundContainer: {
-		flex: 1,
+		flex: 2,
+		height: '100%',
+		width: '100%'
+	},
+	profileImage: {
 		height: '100%',
 		width: '100%'
 	}
