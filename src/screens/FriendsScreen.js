b3252428a404567c1d1d2f50104c5d40ea61eb8e
Orlando Canchola <orcj17544@gmail.com>
2021-11-21 21:10:26 -0600

    ShareScreen created

--- src/screens/FriendsScreen.js
+++ src/screens/FriendsScreen.js
@@ -15,21 +15,37 @@ import {
 	ImageBackground,
 	TouchableOpacity,
 	TextInput,
-	Image
+	Image,
+	Dimensions
 } from 'react-native';
+
+const screenWidth = Dimensions.get('window').width;
+const screenHeight = Dimensions.get('window').height;
 
 const HistoryScreen = () => {
 
 	return (
 		<View style={ styles.container }>
-			<Text>Friends</Text>
+			<View style={{ justifyContent: 'center', marginLeft: 10 }}>
+				<Text style={{ fontSize: 20, fontWeight: 'bold' }}>Share with friends</Text>
+				<Text style={{ fontSize: 15 }}>Share this game with your{'\n'}friends and receive a bonus for{'\n'}each participant</Text>
+			</View>
+			<Image
+				source={require('../../assets/share.jpg')}
+				style={{ height: screenHeight * 0.2, width: screenWidth * 0.3 }}
+				resizeMode="contain"
+			/>
 		</View>
 	);
 };
 
 const styles = StyleSheet.create({
 	container: {
-		flex: 1
+		flexDirection: 'row',
+		backgroundColor: '#f78270',
+		borderRadius: 15,
+		marginHorizontal: 10,
+		marginTop: 10
 	},
 });
 
